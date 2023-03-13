43750d2c6a7824fee010897d029ec3531d9c44fd
Mohamed Awde <116069156+MohamedAwde@users.noreply.github.com>
2023-03-13 16:15:26 +0200

    refactoring home page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { RouterModule } from '@angular/router';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, NotfoundComponent],
+  imports: [CommonModule, RouterModule, NotfoundComponent, HttpClientModule],
   declarations: [
     NavbarComponent,
     HeaderComponent,
